b3fada858eeeb628244785f9942ad54f9cf7e57e
Arkadiy Khorunzhenko <49669372+SkivHisink@users.noreply.github.com>
2023-12-22 01:31:38 +0700

    Added winform project.
    Fixed problems with net48 compatibility

---
+++ WindowsFormsApp1/SFMLcanvas.Designer.cs
@@ -0,0 +1,52 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class SFMLcanvas
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RenderLoopWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // RenderLoopWorker
+            // 
+            this.RenderLoopWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.RenderLoopWorker_DoWork);
+            // 
+            // SFMLcanvas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "SFMLcanvas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker RenderLoopWorker;
+    }
+}
---
+++ WindowsFormsApp1/SFMLcanvas.cs
@@ -0,0 +1,59 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class SFMLcanvas : UserControl
+    {
+        private RenderWindow RendWind;
+
+        public SFMLcanvas()
+        {
+            InitializeComponent();
+        }
+
+        public void StartSLMF()
+        {
+            RenderLoopWorker.RunWorkerAsync(this.Handle); //RenderLoopWorker is a BackgroundWorker
+        }
+
+        private void DrawStuff()
+        {
+            CircleShape CS = new CircleShape(10);
+            CS.FillColor = SFML.Graphics.Color.Magenta;
+            RendWind.Draw(CS);
+        }
+
+        private void RenderLoopWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            RendWind = new RenderWindow((IntPtr)e.Argument);
+            while (RendWind.IsOpen)
+            {
+                RendWind.DispatchEvents();
+                RendWind.Clear(new SFML.Graphics.Color((byte)51, (byte)77, (byte)102));
+                DrawStuff();
+                RendWind.Display();
+            }
+        }
+
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            if (RendWind == null)
+                base.OnPaint(e);
+        }
+
+        protected override void OnPaintBackground(System.Windows.Forms.PaintEventArgs pevent)
+        {
+            if (RendWind == null)
+                base.OnPaintBackground(pevent);
+        }
+    }
+}
